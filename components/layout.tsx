d32134ba370aa1d91a067cf58d0c1bc595e152b0
河野浩生 <61747641koki@gmail.com>
2020-10-04 10:32:55 +0900

    fix step 3

--- components/layout.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './layout.module.css';
-
-function Layout({children}: {children: React.ComponentType}) {
-    return <div className={styles.container}>{children}</div>
-}
-
-export default Layout;
